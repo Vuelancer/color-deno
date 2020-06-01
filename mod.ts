5e5c85c443a082c8066f1dacbdbfab52ac8dfa8a
selvakumardhivakar <selvakumardhivakar@gmail.com>
2020-06-01 13:12:01 +0530

    Add license and gitignore

--- mod.ts
+++ mod.ts
@@ -1,29 +1,29 @@
-const white: String = "\x1b[0m";
+const reset: String = "\x1b[0m";
 const red: String = "\x1b[31m";
 const green: String = "\x1b[32m";
 const yellow: String = "\x1b[33m";
 
 function error(val: any) {
-  return (red + val + white);
+  return (red + val + reset);
 }
 
 function success(val: any) {
-  return (green + val + white);
+  return (green + val + reset);
 }
 
 function warning(val: any) {
-  return (yellow + val + white);
+  return (yellow + val + reset);
 }
 function errorLog(val: any) {
-  console.log(red + val + white);
+  console.log(red + val + reset);
 }
 
 function successLog(val: any) {
-  console.log(green + val + white);
+  console.log(green + val + reset);
 }
 
 function warningLog(val: any) {
-  console.log(yellow + val + white);
+  console.log(yellow + val + reset);
 }
 
 export { error, success, warning, errorLog, successLog, warningLog };
